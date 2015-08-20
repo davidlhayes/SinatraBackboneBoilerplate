5fd939713a648944ba992a50dacd932668414456
David Hayes <davidlynnhayes@att.net>
2015-08-20 15:22:10 -0500

    api created

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'bundler'
 Bundler.require
 
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql',
+  :database => 'instafake'
+)
 get '/' do
 
   erb :index
@@ -36,4 +41,67 @@ get '/5' do
 
   erb :five
 
+end
+
+get '/6' do
+
+  erb :six
+
+end
+get '/7' do
+
+  erb :seven
+
+end
+
+get '/8' do
+
+  erb :eight
+
+end
+
+#### RESTFUL API
+#### /api/instafake
+
+## get
+get '/api/instafake' do
+  InstagramModel.all.to_json
+end
+
+## get by id
+get '/api/instafake/:id' do
+  InstagramModel.find(params[:id]).to_json
+end
+
+## create
+post '/api/instafake' do
+  InstagramModel.create(params).to_json
+end
+
+## update
+put '/api/instafake/:id' do
+  @id = params[:id]
+  @insta = InstagramModel.find(@id)
+  @insta.username = params[:username]
+  @insta.post = params[:post]
+  @insta.description = params[:description]
+  @insta.hashtags = params[:hashtags]
+  @insta.save
+  @insta.to_json
+end
+
+patch '/api/instafake/:id' do
+  @id = params[:id]
+  @insta = InstagramModel.find(@id)
+  @insta.username = params[:username]
+  @insta.post = params[:post]
+  @insta.description = params[:description]
+  @insta.hashtags = params[:hashtags]
+  @insta.save
+  @insta.to_json
+end
+
+## delete
+delete '/api/instafake/:id' do
+  InstagramModel.destroy(params[:id]).to_json
 end
